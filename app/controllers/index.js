39c9eea95b89a03a300dd5e01100640e5afbad76
Patrick Arlt <patrick.arlt@gmail.com>
2015-08-26 21:43:44 -0700

    first

---
+++ app/controllers/index.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+
+var MyCustomElement = Object.create(HTMLElement.prototype);
+
+MyCustomElement.attachedCallback = function () {
+  var self = this;
+  this.addEventListener('click', function () {
+    self.dispatchEvent(new CustomEvent('customevent'));
+  });
+};
+
+document.registerElement('custom-element', {
+  prototype: MyCustomElement
+});
+
+export default Ember.Controller.extend({
+  actions: {
+    handleClick: function (e) {
+      console.log('click');
+    },
+
+    handleCustom: function (e) {
+      console.log('custom');
+    }
+  }
+});
